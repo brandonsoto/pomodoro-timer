67a4126ab364c9cbaf98467ece1ad0ffc7bfb978
Brandon Soto <brandon.soto09@gmail.com>
2018-01-14 20:52:06 -0800

    Integrate DocumentDiffs into main

--- extension.js
+++ extension.js
@@ -1,17 +1,27 @@
 const vscode = require('vscode');
 const pomodoro = require('./pomodoro');
+const diff = require('./diff');
 
 const pomodoroTimer = new pomodoro.PomodoroTimer();
+const documentDiffs = new diff.DocumentDiffs();
 
 function activate(context) {
     console.log(pomodoroTimer.name + ' now active');
 
     let startTimer = vscode.commands.registerCommand('extension.startTimer', () => {
         pomodoroTimer.start();
+        documentDiffs.clear();
     });
     let stopTimer = vscode.commands.registerCommand('extension.stopTimer', () => {
         pomodoroTimer.stop();
+        console.log("total changes =" + documentDiffs.total());
+        documentDiffs.clear();
     });
+
+    vscode.workspace.onDidChangeTextDocument((ev)=>{
+        documentDiffs.update(ev.document);
+    });
+
     // TODO: is there a cleaner way to accomplish this?
     let showAll = vscode.commands.registerCommand('extension.showAll', () => {
         vscode.window.showQuickPick(['Start Timer', 'Stop Timer'])
